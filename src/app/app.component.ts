b3afa2678a4949a771bb2a61a412cf1af8882515
fatih efkan <mfefkan@hotmail.com>
2023-09-15 12:00:12 +0300

    Structural Directive örnekleri yapıldı.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<!--<div *appCustomif="true"> MERHABA </div> <ul> <li *appCustomfor="10; let i" >Muhammed {{i}}</li> --> <ul> <li *appCustomfor="names; let name; let index = index" >Muhammed {{name}} ---- {{index}}</li>'
+
 })
 export class AppComponent {
   title = 'Structural-Directive';
+  names: string[] = ["Ahmet", "Murtaza", "Rıfkı", "Şuayp", "Hilmi", "Hüseyin"];
 }
---
+++ src/app/directives/customif.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, Input, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appCustomif]'
+})
+export class CustomifDirective {
+
+  constructor(
+    private templateRef : TemplateRef<any>,
+    private viewContainerRef: ViewContainerRef
+  ) { }
+
+  @Input() set appCustomif(value : boolean){
+
+    if(value)
+    {
+        this.viewContainerRef.createEmbeddedView(this.templateRef)
+    }
+    else
+    {
+      this.viewContainerRef.clear()
+    }
+
+  }
+
+}
